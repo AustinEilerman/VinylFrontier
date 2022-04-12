31545d11b9699c3e750a69e1a20475021f829b2c
Tyler Bosh <tylermarkleybosh@gmail.com>
2022-04-12 09:59:37 -0400

    Updated data.sql and schema.sql

--- java/database/data.sql
+++ java/database/data.sql
@@ -1,2 +1,4 @@
 INSERT INTO records(record_user_id, record_title, record_artist, record_release_date, record_genre, record_length_in_sec) VALUES (1, 'Dark Side Of The Moon', 'Pink Floyd', '1973-3-1', 'Psychedelic Rock', 2585);
-INSERT INTO collections(collection_user_id, collection_record_id, is_public, collection_name) VALUES (1, 1, 'true', 'Best Albums Of All Time');
+INSERT INTO collections(collection_user_id, is_public, collection_name) VALUES (1, 'true', 'Best Albums Of All Time');
+INSERT INTO records(record_user_id, record_title, record_artist, record_release_date, record_genre, record_length_in_sec) VALUES (1, 'The Beatles (White Album)', 'The Beatles', '1968-11-22', 'Pop', 5580);
+INSERT INTO collections(collection_user_id, is_public, collection_name) VALUES (1, 'false', 'Worst Albums Of All Time');
--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,10 +1,10 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS collections;
-DROP SEQUENCE IF EXISTS seq_collection_id;
-
 DROP TABLE IF EXISTS records;
 DROP SEQUENCE IF EXISTS seq_record_id;
+
+DROP TABLE IF EXISTS collections;
+DROP SEQUENCE IF EXISTS seq_collection_id;
 
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS seq_user_id;
@@ -32,32 +32,33 @@ CREATE TABLE users (
 	username varchar(50) UNIQUE NOT NULL,
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
+	is_premium BOOLEAN DEFAULT 'false',
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
-);
-
-CREATE TABLE records (
-	record_id int DEFAULT nextval('seq_record_id'::regclass) NOT NULL,
-	record_user_id int NOT NULL,
-	record_title varchar(50) NOT NULL,
-	record_artist varchar(50) NOT NULL,
-	record_release_date DATE NOT NULL,
-	record_genre varchar(50) NOT NULL,
-	record_user_description varchar(255),
-	record_user_rating int,
-	record_length_in_sec int,
-	CONSTRAINT fk_user_id FOREIGN KEY (record_user_id) REFERENCES users(user_id),
-	CONSTRAINT PK_record_id PRIMARY KEY (record_id)
 );
 
 CREATE TABLE collections (
 	collection_id int DEFAULT nextval('seq_collection_id'::regclass) NOT NULL,
 	collection_user_id int NOT NULL,
-	collection_record_id int NOT NULL,
 	is_public boolean DEFAULT 'false',
 	collection_name varchar(50) NOT NULL,
-	CONSTRAINT fk_record_id FOREIGN KEY (collection_record_id) REFERENCES records(record_id),
 	CONSTRAINT fk_user_id FOREIGN KEY (collection_user_id) REFERENCES users(user_id),
 	CONSTRAINT PK_collection_id PRIMARY KEY (collection_id)
+);
+
+CREATE TABLE records (
+	record_id int DEFAULT nextval('seq_record_id'::regclass) NOT NULL,
+	record_user_id int NOT NULL,
+	record_collection_id int,
+	record_title varchar(50) NOT NULL,
+	record_artist varchar(50) NOT NULL,
+	record_release_date DATE,
+	record_genre varchar(50) NOT NULL,
+	record_user_description varchar(255),
+	record_user_rating int,
+	record_length_in_sec int,
+	CONSTRAINT fk_user_id FOREIGN KEY (record_user_id) REFERENCES users(user_id),
+	CONSTRAINT fk_collection_id FOREIGN KEY (record_collection_id) REFERENCES collections(collection_id),
+	CONSTRAINT PK_record_id PRIMARY KEY (record_id)
 );
 
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
