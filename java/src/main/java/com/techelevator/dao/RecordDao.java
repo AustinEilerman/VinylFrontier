28b6a7b503e54eb93979c19ec0b4dbd69f71c9e9
Jeremy <davidson.jw@gmail.com>
2022-04-14 10:24:33 -0400

    added list of records to /records

--- java/src/main/java/com/techelevator/dao/RecordDao.java
+++ java/src/main/java/com/techelevator/dao/RecordDao.java
@@ -2,9 +2,13 @@ package com.techelevator.dao;
 
 import com.techelevator.model.Record;
 
+import java.util.List;
+
 public interface RecordDao {
 
     Record getRecord(int recordId);
 
     Record createRecord(Record newRecord);
+
+    List<Record> findAll();
 }
