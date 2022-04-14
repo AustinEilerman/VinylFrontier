b8f3c227e960434ca4500a640e6029d102ab51b9
Jay Chen <jaycsun1@gmail.com>
2022-04-14 17:54:57 -0400

    Deleted record_collection_id from table since we no longer need it

--- java/database/data.sql
+++ java/database/data.sql
@@ -2,3 +2,13 @@ INSERT INTO records(record_user_id, record_title, record_artist, record_release_
 INSERT INTO collections(collection_user_id, is_public, collection_name) VALUES (1, 'true', 'Best Albums Of All Time');
 INSERT INTO records(record_user_id, record_title, record_artist, record_release_date, record_genre, record_length_in_sec) VALUES (1, 'The Beatles (White Album)', 'The Beatles', '1968-11-22', 'Pop', 5580);
 INSERT INTO collections(collection_user_id, is_public, collection_name) VALUES (1, 'false', 'Worst Albums Of All Time');
+
+SELECT * FROM records;
+
+SELECT * FROM users;
+
+SELECT *
+FROM records
+JOIN records_collections ON records.record_id = records_collections.record_id
+JOIN collections ON collections.collection_id = records_collections.collection_id
+WHERE record_user_id = 1;
